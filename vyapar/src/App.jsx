70052fb9bb30998eee648346d69cc37043fe4784
akash0751 <akashsofficial05@gmail.com>
2025-07-23 10:29:14 +0530

    added

--- vyapar/src/App.jsx
+++ vyapar/src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import Signup from "./components/Signup";
 import AdminLoginPage from "./components/AdminLoginPage";
-import AdminSignup from "./components/AdminSignup";
-import Details from "./components/Details";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import Product from "./components/Product";
-import Cart from "./components/Cart";
-import { CartProvider } from "./context/CartContext";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/LoginPage.css";
-import OtpVerificationPage from "./components/OtpVerificationPage";
-import SetNewPasswordPage from "./components/SetNewPasswordPage";
-import ForgotOtp from "./components/ForgotOtp";
-import EmailComponent from "./components/EmailComponent";
 import AdminAddProduct from "./components/AdminAddProduct";
 import AdminViewProduct from "./components/AdminViewProduct";
 import AdminHome from "./components/AdminHome";
@@ -25,7 +11,7 @@ import AdminProductMetaView from "./components/AdminProductMetaView";
 
 const App = () => {
   return (
-    <CartProvider>
+   
       <Router>
         <Routes>
           <Route path="/" element={<AdminLoginPage />} />
@@ -37,7 +23,7 @@ const App = () => {
           <Route path="/viewMeta" element={<AdminProductMetaView />} />
         </Routes>
       </Router>
-    </CartProvider>
+    
   );
 };
 
